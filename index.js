c41bf0209ca51178c4ff19a003c2bcafb58ab65f
waqarsaba167-jpg <waqarsaba167@gmail.com>
2026-09-03 18:43:59 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(express.json());
 app.use(cors());
+app.use(express.static(__dirname));app.use(cors());
 
 const JWT_SECRET = process.env.JWT_SECRET || 'taskconnect_secret_key_2026';
 
